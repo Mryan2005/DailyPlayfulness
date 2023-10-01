abbb79e6a56c2600954c8d6614d9d45484108cae
Mryan2005 <A2564011261@163.com>
2023-10-01 11:53:59 +0800

    add

---
+++ LaughAndPlay/Check if it is a prime.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    int n, i = 1;
+    scanf("%d", &n);
+    while ( n > 1 ){
+        if ( n / i == 1 ){
+            if ( i == n ){
+                printf("%d是素数\n", n);
+                break;
+            } else if ( i == 1 ){
+                printf("%d可被1整除\n", n);
+            } else {
+                printf("%d不是素数\n", n);
+                break;
+            }    
+        }
+        i++;
+    }
+}
